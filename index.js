43c7313e62d8c344a26f47a7e179f9526378f377
Elias Rivero <eliasrivero814@gmail.com>
2025-04-04 14:58:43 -0300

    add js

---
+++ index.js
@@ -0,0 +1,17 @@
+let usuario;
+usuario = "venus"
+usuario = "pancho"
+console.log(usuario);
+let edad;
+edad = "3 meses"
+console.log(edad);
+let cliente;
+cliente = "pepe"
+cliente= "venus otra vez"
+console.log(cliente);
+const hoy = 4; 
+console.log(hoy);
+console.log("pancho me debe 3 pesos")
+
+const titulo = document.getElementById("titulo")
+titulo.textContent = "texto arreglado"
